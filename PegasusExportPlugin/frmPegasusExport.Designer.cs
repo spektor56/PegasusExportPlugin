b7be5995f73ea00ed3a85cebf847ed586cfc17f8
Brett Sanderson <l-brett-l@hotmail.com>
2019-06-19 01:02:34 -0400

    Add project files.

---
+++ PegasusExportPlugin/frmPegasusExport.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PegasusExportPlugin
+{
+    partial class frmPegasusExport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.fbdExportFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtExportPath = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.chkMetaData = new System.Windows.Forms.CheckBox();
+            this.chkRoms = new System.Windows.Forms.CheckBox();
+            this.chkAssets = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(12, 94);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(478, 23);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "&Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.BtnExport_Click);
+            // 
+            // txtExportPath
+            // 
+            this.txtExportPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtExportPath.Location = new System.Drawing.Point(90, 12);
+            this.txtExportPath.Name = "txtExportPath";
+            this.txtExportPath.ReadOnly = true;
+            this.txtExportPath.Size = new System.Drawing.Size(319, 20);
+            this.txtExportPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Export Folder:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(415, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(15, 61);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(475, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // chkMetaData
+            // 
+            this.chkMetaData.AutoSize = true;
+            this.chkMetaData.Checked = true;
+            this.chkMetaData.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkMetaData.Location = new System.Drawing.Point(15, 39);
+            this.chkMetaData.Name = "chkMetaData";
+            this.chkMetaData.Size = new System.Drawing.Size(104, 17);
+            this.chkMetaData.TabIndex = 5;
+            this.chkMetaData.Text = "Export Metadata";
+            this.chkMetaData.UseVisualStyleBackColor = true;
+            // 
+            // chkRoms
+            // 
+            this.chkRoms.AutoSize = true;
+            this.chkRoms.Checked = true;
+            this.chkRoms.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRoms.Location = new System.Drawing.Point(221, 38);
+            this.chkRoms.Name = "chkRoms";
+            this.chkRoms.Size = new System.Drawing.Size(86, 17);
+            this.chkRoms.TabIndex = 6;
+            this.chkRoms.Text = "Export Roms";
+            this.chkRoms.UseVisualStyleBackColor = true;
+            // 
+            // chkAssets
+            // 
+            this.chkAssets.AutoSize = true;
+            this.chkAssets.Checked = true;
+            this.chkAssets.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAssets.Location = new System.Drawing.Point(125, 38);
+            this.chkAssets.Name = "chkAssets";
+            this.chkAssets.Size = new System.Drawing.Size(90, 17);
+            this.chkAssets.TabIndex = 7;
+            this.chkAssets.Text = "Export Assets";
+            this.chkAssets.UseVisualStyleBackColor = true;
+            // 
+            // frmPegasusExport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 129);
+            this.Controls.Add(this.chkAssets);
+            this.Controls.Add(this.chkRoms);
+            this.Controls.Add(this.chkMetaData);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtExportPath);
+            this.Controls.Add(this.btnExport);
+            this.MinimumSize = new System.Drawing.Size(330, 168);
+            this.Name = "frmPegasusExport";
+            this.ShowIcon = false;
+            this.Text = "Pegasus Export";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.FolderBrowserDialog fbdExportFolder;
+        private System.Windows.Forms.MaskedTextBox txtExportPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.CheckBox chkMetaData;
+        private System.Windows.Forms.CheckBox chkRoms;
+        private System.Windows.Forms.CheckBox chkAssets;
+    }
+}
---
+++ PegasusExportPlugin/frmPegasusExport.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PegasusExportPlugin.Properties;
+using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+
+namespace PegasusExportPlugin
+{
+    public partial class frmPegasusExport : Form, ISystemMenuItemPlugin
+    {
+        public frmPegasusExport()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption => "Pegasus Export";
+
+        public Image IconImage => Resources.favicon96;
+
+        public bool ShowInLaunchBox => true;
+
+        public bool ShowInBigBox => false;
+
+        public bool AllowInBigBoxWhenLocked => false;
+
+        public void OnSelected()
+        {
+            this.Show();
+        }
+
+        private async void BtnExport_Click(object sender, System.EventArgs e)
+        {
+            if(!chkAssets.Checked && !chkRoms.Checked && !chkMetaData.Checked)
+            {
+                MessageBox.Show("Please select at least one item to export.");
+                return;
+            }
+
+            var selectedFolder = fbdExportFolder.SelectedPath;
+
+            if (string.IsNullOrWhiteSpace(selectedFolder) ||  !Directory.Exists(selectedFolder))
+            {
+                MessageBox.Show("Please select a valid location.");
+                return;
+            }
+            
+            progressBar.Value = 0;
+            await Task.Run(() => {
+                int progress = 0;
+                var dataManager = PluginHelper.DataManager;
+                var games = dataManager.GetAllGames();
+                var numberOfGames = games.Length;
+                var gamesByPlatform = games.GroupBy(game => game.Platform);
+
+                foreach (var gamePlatform in gamesByPlatform)
+                {
+                    var platformFolderName = Unbroken.FileHelper.CoerceValidFileName(gamesByPlatform.First().Key);
+                    var platformPath = Path.Combine(selectedFolder, platformFolderName);
+                    Directory.CreateDirectory(platformPath);
+                    var stringBuilder = new StringBuilder();
+
+                    foreach (var game in gamePlatform)
+                    {
+                        progress++;
+                        BeginInvoke(new MethodInvoker(() => { progressBar.Value = (int)(((double)progress / (double)numberOfGames) * 100); }));
+
+                        if(chkMetaData.Checked)
+                        {
+                            if(string.IsNullOrWhiteSpace(game.Title))
+                            {
+                                continue;
+                            }
+                            stringBuilder.AppendLine($"game: {game.Title}");
+
+                            if (!string.IsNullOrWhiteSpace(game.ApplicationPath))
+                            {
+                                var file = Path.GetFileName(game.ApplicationPath);
+                                stringBuilder.AppendLine($"file: {file}");
+                            }
+
+                            if (!string.IsNullOrWhiteSpace(game.Developer))
+                            {
+                                stringBuilder.AppendLine($"developer: {game.Developer}");
+                            }
+
+                            if (!string.IsNullOrWhiteSpace(game.Publisher))
+                            {
+                                stringBuilder.AppendLine($"publisher: {game.Publisher}");
+                            }
+
+                            foreach (var genre in game.Genres)
+                            {
+                                if (!string.IsNullOrWhiteSpace(genre))
+                                {
+                                    stringBuilder.AppendLine($"genre: {genre}");
+                                }
+                            }
+
+                            if (!string.IsNullOrWhiteSpace(game.Notes))
+                            {
+                                stringBuilder.AppendLine($"description: {game.Notes}");
+                            }
+
+                            if (game.ReleaseDate != null)
+                            {
+                                stringBuilder.AppendLine($"release: {((DateTime)game.ReleaseDate).ToString("yyyy-MM-dd")}");
+                            }
+
+                            if (game.CommunityStarRatingTotalVotes > 0)
+                            {
+                                var rating = (int)((game.CommunityStarRating / (float)5) * 100);
+                                stringBuilder.AppendLine($"rating: {rating}");
+                            }
+
+                            stringBuilder.AppendLine();
+                        }
+
+                        if(chkAssets.Checked)
+                        {
+                            var mediaFolder = Path.Combine(platformPath, "media", Path.GetFileNameWithoutExtension(game.ApplicationPath));
+                            Directory.CreateDirectory(mediaFolder);
+
+                            var images = game.GetAllImagesWithDetails();
+                            foreach (var image in images)
+                            {
+                                if (!string.IsNullOrWhiteSpace(image.FilePath) && File.Exists(image.FilePath))
+                                {
+                                    if (image.ImageType == Unbroken.LaunchBox.Properties.Strings.ImageTypeBoxFront)
+                                    {
+                                        File.Copy(image.FilePath, Path.Combine(mediaFolder, "boxFront" + Path.GetExtension(image.FilePath)), true);
+                                    }
+                                    if (image.ImageType == Unbroken.LaunchBox.Properties.Strings.ImageTypeClearLogo)
+                                    {
+                                        File.Copy(image.FilePath, Path.Combine(mediaFolder, "logo" + Path.GetExtension(image.FilePath)), true);
+                                    }
+                                    if (image.ImageType == Unbroken.LaunchBox.Properties.Strings.ImageTypeFanartBackground)
+                                    {
+                                        File.Copy(image.FilePath, Path.Combine(mediaFolder, "background" + Path.GetExtension(image.FilePath)), true);
+                                    }
+                                }
+                            }
+                            var video = game.GetVideoPath(false);
+                            if (!string.IsNullOrWhiteSpace(video) && File.Exists(video))
+                            {
+                                File.Copy(video, Path.Combine(mediaFolder, "video" + Path.GetExtension(video)), true);
+                            }
+                        }
+
+                        if(chkRoms.Checked)
+                        {
+                            File.Copy(game.ApplicationPath, Path.Combine(platformPath, Path.GetFileName(game.ApplicationPath)), true); 
+                        }
+                        
+                    }
+                    if(chkMetaData.Checked)
+                    {
+                        File.WriteAllText(Path.Combine(platformPath, "metadata.pegasus.txt"), stringBuilder.ToString());
+                    }
+                }
+
+            });
+           
+
+        }
+
+        private void BtnBrowse_Click(object sender, System.EventArgs e)
+        {
+            if(fbdExportFolder.ShowDialog() == DialogResult.OK)
+            {
+                txtExportPath.Text = fbdExportFolder.SelectedPath;
+            }
+        }
+    }
+}
